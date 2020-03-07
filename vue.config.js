05c925e46da157f1406856db2e9aa2726521fde9
FKD13 <francisklinck@gmail.com>
2020-03-07 14:09:24 +0100

    add prefix for deployment

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     transpileDependencies: ["vuetify"],
-    publicPath: "/"
+    publicPath: process.env.NODE_ENV == 'production' ? '/frontend/production' : process.env.NODE_ENV == 'development' ? '/frontend/development' : ''
 };
