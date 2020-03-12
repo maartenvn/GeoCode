c86c35ffadfbb1d237fac41b480fd2b28c1be32d
maartenvn <maartenvn.eu@gmail.com>
2020-03-12 18:12:05 +0100

    Fix incorrect MDI icon package

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
 
-import "material-design-icons-iconfont/dist/material-design-icons.css";
+import "@mdi/font/css/materialdesignicons.css";
 import "typeface-roboto/index.css";
 
 Vue.use(Vuetify);
