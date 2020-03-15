e74c597956aa3051eddfe0dcf592642cc0d41bc7
maartenvn <maartenvn.eu@gmail.com>
2020-03-15 18:51:09 +0100

    Add support for GZIP-compression

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,14 @@
+/* eslint-disable */
+
 module.exports = {
     transpileDependencies: ["vuetify"],
     publicPath: process.env.VUE_APP_PUBLIC_PATH
         ? process.env.VUE_APP_PUBLIC_PATH
-        : ""
+        : "",
+    chainWebpack(config) {
+        config.plugins.delete("prefetch");
+
+        // and this line
+        config.plugin("CompressionPlugin").use(CompressionPlugin);
+    }
 };
