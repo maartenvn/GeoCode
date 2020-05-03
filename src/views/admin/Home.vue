ad09d47ca92a69e530e86b27b4e11994fcd8e84a
Hannes <7892467+hannes-dev@users.noreply.github.com>
2020-05-03 07:06:15 +0200

    broken commit

--- src/views/admin/Home.vue
+++ src/views/admin/Home.vue
@@ -1,5 +1,27 @@
 <template>
     <v-container class="container--small">
-        Hello
+        <p v-for="report in reports" :key="report.id">
+            {{ report.location.name }}
+        </p>
     </v-container>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { StoreGetter } from "@/store/decorators/StoreGetterDecorator";
+import AdminNavigation from "@/components/admin/AdminNavigation.vue";
+import AdminService from "@/api/services/AdminService";
+import { HandleRequest } from "@/api/decorators/HandleRequestDecorator";
+import { EchoPromise } from "echofetch";
+import { RequestHandler } from "@/api/RequestHandler";
+
+@Component({
+    components: {
+        AdminNavigation
+    }
+})
+export default class Home extends Vue {
+    reports = AdminService.getAll().then((data: string) => console.log(data));
+    // console.log(reports);
+}
+</script>
