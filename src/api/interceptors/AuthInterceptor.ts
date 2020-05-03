bae43adf4341a2f71fed473b8bb68d2309bbc851
maartenvn <maartenvn.eu@gmail.com>
2020-05-03 14:24:24 +0200

    Add caching of backend responses

--- src/api/interceptors/AuthInterceptor.ts
+++ src/api/interceptors/AuthInterceptor.ts
@@ -1,10 +1,19 @@
 import { EchoServiceInterceptor } from "echofetch/dist/types/service/EchoServiceInterceptor";
 import { EchoError, EchoRequest, EchoResponse } from "echofetch";
+import { setupCache } from "axios-cache-adapter";
+
+// Create a cache for storing the network results.
+const cache = setupCache({
+    maxAge: 15 * 60 * 1000,
+});
 
 export class AuthInterceptor implements EchoServiceInterceptor {
     onRequest(request: EchoRequest): EchoRequest {
         // Send credentials with every request.
         request.withCredentials = true;
+
+        // Cache every request.
+        request.adapter = cache.adapter;
 
         return request;
     }
