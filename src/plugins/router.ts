bc0a41cb8c531c7184c1c58171055dc7fde40339
maartenvn <maartenvn.eu@gmail.com>
2020-05-03 21:18:08 +0200

    Fix wrong order in navigation

--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -37,7 +37,7 @@ const routes = [
         name: "Leaderboards",
         component: () => import("../views/Leaderboards.vue"),
     },
-        
+
     {
         path: "/tours",
         name: "Tours",
