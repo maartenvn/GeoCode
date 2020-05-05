857ce00de610d36166ae418340a33518ad2c2712
Hannes <7892467+hannes-dev@users.noreply.github.com>
2020-05-05 23:22:47 +0200

    add resolved field to Report model

--- src/api/models/Report.ts
+++ src/api/models/Report.ts
@@ -6,6 +6,7 @@ export default class Report {
     id: number;
     message: string;
     reason: string;
+    resolved: boolean;
     image: ImageIdWrapper;
     creator: UserIdWrapper;
     location: LocationIdWrapper;
